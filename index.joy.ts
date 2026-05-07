92608fa325a4e0ec8223c8ece52d4aef18d3872f
Bill DeStein <you@example.com>
2026-05-07 11:04:59 -0400

    More work

--- index.joy.ts
+++ index.joy.ts
@@ -19,34 +19,34 @@ learn(frames)
 
 learn(framesTest)
 
-execute(`
-    before proceeding, let me know if you have enough details to build the 
-    backend repo, the common repo, and the backend-test repo')
-`)
+// execute(`
+//     before proceeding, let me know if you have enough details to build the 
+//     backend repo, the common repo, and the backend-test repo')
+// `)
 
-// common
-execute(`
-    create the 'lucy/common' subdirectory, 
-    and build the common repo in it.
-`)
+// // common
+// execute(`
+//     create the 'lucy/common' subdirectory, 
+//     and build the common repo in it.
+// `)
 
-// backend
-execute(`
-    create the 'lucy/backend' subdirectory, 
-    and build the backend repo in it.
-`)
+// // backend
+// execute(`
+//     create the 'lucy/backend' subdirectory, 
+//     and build the backend repo in it.
+// `)
 
-// backend-test
-execute(`
-    create the 'lucy/backend-test' subdirectory, 
-    and build the backend-test repo in it.
-`)
+// // backend-test
+// execute(`
+//     create the 'lucy/backend-test' subdirectory, 
+//     and build the backend-test repo in it.
+// `)
 
-// frames
-execute(`
-    delete and recreate the 'lucy/frontend/frames' subdirectory, 
-    and build the frames library in it.
-`)
+// // frames
+// execute(`
+//     delete and recreate the 'lucy/frontend/frames' subdirectory, 
+//     and build the frames library in it.
+// `)
 
 // frames-test
 execute(`
