d62dc2576f7fbde9a9855fea944ae76018788344
Bill DeStein <you@example.com>
2026-05-12 11:11:20 -0400

    More work

--- lucy/frontend/src/Frames/index.ts
+++
@@ -1,4 +0,0 @@
-export { Frame } from './Frame'
-export { FrameHeaderButtonComponent } from './FrameHeaderButtonComponent'
-export { addFrame, removeFrame, initCanvas } from './canvas'
-export type { FrameProps } from './types'
--- lucy/frontend/src/Frames/zindex.ts
+++
@@ -1,9 +0,0 @@
-let currentMaxZ = 10
-
-export function getNextZ(): number {
-    return ++currentMaxZ
-}
-
-export function getCurrentMax(): number {
-    return currentMaxZ
-}
